a1c24855ef145e333af5b970d1f2c3f1ed27688e
Gbeengah <97206259+Gbeengah@users.noreply.github.com>
2024-03-25 00:23:38 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Set up middleware for parsing request bodies
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Sample data - Todo list
+let todos = [];
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Welcome to the Todo list application!');
+});
+
+// Get all todos
+app.get('/todos', (req, res) => {
+  res.json(todos);
+});
+
+// Create a new todo
+app.post('/todos', (req, res) => {
+  const { title } = req.body;
+  const newTodo = { id: todos.length + 1, title };
+  todos.push(newTodo);
+  res.status(201).json(newTodo);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Todo app listening at http://localhost:${port}`);
+});
